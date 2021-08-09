aacced1098910404cfaf2d52efdd04fc656e5032
Parneet Raghuvanshi <parneetraghuvanshi@gmail.com>
2021-08-09 21:24:20 +0530

    Fixed Communities String

--- lib/user/screens/user_dashboard_pages.dart/create_activity_screen.dart
+++ lib/user/screens/user_dashboard_pages.dart/create_activity_screen.dart
@@ -1,5 +1,4 @@
 import 'package:buddy/chat/models/chat_list_provider.dart';
-import 'package:buddy/chat/screens/user_chat_list.dart';
 import 'package:buddy/components/custom_snackbar.dart';
 import 'package:buddy/components/rounded_input_field.dart';
 import 'package:buddy/components/textarea.dart';
@@ -99,10 +98,10 @@ class _CreateActivityScreenState extends State<CreateActivityScreen> {
         Provider.of<ChatListProvider>(context, listen: false).allChatList;
     _chatList.forEach((element) {
       if (element.user == auth.currentUser!.uid) {
-        _allCommunities += splitCode + element.chid;
+        _allCommunities +=
+            _allCommunities == '' ? element.chid : splitCode + element.chid;
       }
     });
-    _allCommunities.replaceFirst(splitCode, '');
     print(_allCommunities);
   }
 
@@ -124,9 +123,11 @@ class _CreateActivityScreenState extends State<CreateActivityScreen> {
                     builder: (context) {
                       return BottomSheet();
                     }).then((value) {
-                  setState(() {
-                    selectedShareType = value;
-                  });
+                  if (value != null) {
+                    setState(() {
+                      selectedShareType = value;
+                    });
+                  }
                 });
               },
               child: Container(
@@ -364,7 +365,7 @@ class _CreateActivityScreenState extends State<CreateActivityScreen> {
         creatorId: _user!.uid,
         creatorName: _creatorName,
         creatorClg: _creatorClg,
-        communities: '',
+        communities: _allCommunities,
       );
 
       final isEditing = widget.event != null;
